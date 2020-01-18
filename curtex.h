66e83b0c2779b5b74774d866b2fc0dd17a0bfed0
hejohns <hejohns@umich.edu>
2020-01-18 02:31:56 -0500

    need to define screen_buffer functions

--- curtex.h
+++ curtex.h
@@ -12,6 +12,7 @@
 sigset_t sigwinch_mask;
 int ROWS_, COLS_;
 
+
 /* REQUIRES: yes
  * MODIFIES: yes
  * EFFECTS: yes
@@ -27,4 +28,75 @@ void sigwinch_handler();
  * NOTES: Run at beginning of main.
  */
 void sigwinch_initialize();
+
+/* REQUIRES: none
+ * MODIFIES: none
+ * EFFECTS: none
+ *
+ * NOTES:
+ */
+void screen_buffer_push(char* command);
+
+/* REQUIRES: none
+ * MODIFIES: none
+ * EFFECTS: none
+ *
+ * NOTES:
+ */
+void screen_buffer_pop(char* command);
+
+/* REQUIRES: none
+ * MODIFIES: none
+ * EFFECTS: none
+ *
+ * NOTES:
+ */
+size_t screen_buffer_size();
+
+/* REQUIRES: none
+ * MODIFIES: none
+ * EFFECTS: none
+ *
+ * NOTES:
+ */
+char* screen_buffer_peek();
+
+/* REQUIRES: none
+ * MODIFIES: none
+ * EFFECTS: none
+ *
+ * NOTES:
+ */
+void screen_buffer_clear();
+
+/* REQUIRES: none
+ * MODIFIES: none
+ * EFFECTS: none
+ *
+ * NOTES:
+ */
+void screen_buffer_erase(size_t index);
+
+typedef struct{
+    //public
+    void (*push)(char*);
+    void (*pop)(char*);
+    size_t (*size)();
+    char* (*peek)();
+    void (*clear)();
+    void (*erase)(size_t);
+    //private
+    char queue[80][80];
+    size_t rows;
+} screen_buffer;
+
+screen_buffer buffer={
+    .push=&screen_buffer_push,
+    .pop=&screen_buffer_pop,
+    .size=&screen_buffer_size,
+    .peek=&screen_buffer_peek,
+    .clear=&screen_buffer_clear,
+    .erase=&screen_buffer_erase,
+    .rows=(size_t)0
+};
 #endif /* CURTEX_H */
