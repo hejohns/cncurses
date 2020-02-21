6c1597102aafbc399fab5efee5c71abcca8b74f6
Johnson <hejohns@umich.edu>
2020-02-21 02:47:13 -0500

    total mess but actually mostly works

--- gcon.c
+++ gcon.c
@@ -2,13 +2,65 @@
 #include <stdio.h>
 #include <curses.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <ctype.h>
+#include <time.h>
+#include <stdarg.h>
 
-#define EXIT_KEY KEY_HOME
+#define EXIT_KEY '|'
+void stuff(char* cstr, int num, ...){
+    va_list args;
+    va_start(args, num);
+    for(int i=0; i<num; i++){
+        cstr[strlen(cstr)-2] = va_arg(args, int);
+        system(cstr);
+    }
+    va_end(args);
+}
 
 int main(int argc, char** argv){
-    //check for existing bt.tmp and infob.tmp
+    if(argc != 3){
+        fprintf(stderr, "incorrect usage\n");
+        exit(EXIT_FAILURE);
+    }
+    unsigned int uniq = time(NULL);
+    size_t sz = 80;
+    char* cstr = malloc(sz);
+    int len = snprintf(cstr, sz, "screen -S %s-%d -dm bash -i", argv[1], uniq);
+    if(len > sz){
+        char* tmp = realloc(cstr, len+1);
+        if(tmp == NULL){
+            fprintf(stderr, "realloc failure\n");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            sz = len+1;
+            cstr = tmp;
+            sprintf(cstr, "screen -S %s-%d -dm bash -i", argv[1], uniq);
+        }
+    }
+    system(cstr);
+    len = snprintf(cstr, sz, "screen -S %s-%d -p 0 -X stuff \"c\"", argv[1], uniq);
+    if(len > sz){
+        char* tmp = realloc(cstr, len+1);
+        if(tmp == NULL){
+            fprintf(stderr, "realloc failure\n");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            sz = len+1;
+            cstr = tmp;
+            sprintf(cstr, "screen -S %s-%d -p 0 -X stuff \"c\"", argv[1], uniq);
+        }
+    }
+    stuff(cstr, 4, 'g', 'd', 'b', ' ');
+    for(int i=0; i<strlen(argv[2]); i++){
+        stuff(cstr, 1, argv[2][i]);
+    }
+    stuff(cstr, 32, ' ', '2', '>', '&', '1', ' ', '|', 't', 'e', 'e', ' ', '.', '/', 'b', 't', '.', 't', 'm', 'p', '>', '.', '/', 'i', 'n', 'f', 'o', 'b', '.', 't', 'm', 'p','\n');
     initscr();
     timeout(-1);
     curs_set(0);
@@ -17,20 +69,75 @@ int main(int argc, char** argv){
     keypad(stdscr, TRUE);
     clear();
     refresh();
+    int btfd;
+    int infobfd;
+    if(btfd = open("./bt.tmp", O_CREAT | O_TRUNC | O_WRONLY | O_SYNC, 0644), btfd == -1){
+        fprintf(stderr, "open() failed\n");
+        endwin();
+        exit(EXIT_FAILURE);
+    }
+    if(infobfd = open("./infob.tmp", O_CREAT | O_TRUNC | O_WRONLY | O_SYNC, 0644), infobfd == -1){
+        fprintf(stderr, "open() failed\n");
+        endwin();
+        exit(EXIT_FAILURE);
+    }
     char* screenStuff = malloc(strlen("screen -S ") + strlen(argv[1]) + strlen(" -p 0 -X stuff \"c\"") + 1);
-    int ch;
+    int ch = 0;
     while((ch = getch()) != EXIT_KEY){
-        switch(ch){
-            KEY_BACKSPACE:
-                sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '');
-                system(screenStuff);
-                break;
-            default:
-                sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], (char)ch);
-                system(screenStuff);
-                break;
+        if(ch == KEY_BACKSPACE){
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '');
+            system(screenStuff);
+            stuff(cstr, 1, '');
+        }
+        else if(ch == KEY_LEFT){
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '');
+            system(screenStuff);
+            stuff(screenStuff, 2, 'O', 'D');
+            stuff(cstr, 3, '', 'O', 'D');
+        }
+        else if(ch == KEY_RIGHT){
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '');
+            system(screenStuff);
+            stuff(screenStuff, 2, 'O', 'C');
+            stuff(cstr, 3, '', 'O', 'C');
+        }
+        else if(ch == KEY_UP){
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '');
+            system(screenStuff);
+            stuff(screenStuff, 2, 'O', 'A');
+            stuff(cstr, 3, '', 'O', 'A');
+        }
+        else if(ch == KEY_DOWN){
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '');
+            system(screenStuff);
+            stuff(screenStuff, 2, 'O', 'B');
+            stuff(cstr, 3, '', 'O', 'B');
+        }
+        else if(ch == '\n'){
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], '\n');
+            system(screenStuff);
+            stuff(cstr, 1, '\n');
+            truncate("./bt.tmp", 0);
+            stuff(cstr, 3, 'b', 't', '\n');
+            //truncate("./infob.tmp", 0);
+            //stuff(cstr, 6, 'i', 'n', 'f', 'o', ' ', 'b', '\n');
+        }
+        else{
+            sprintf(screenStuff, "screen -S %s -p 0 -X stuff \"%c\"", argv[1], (char)ch);
+            system(screenStuff);
+            stuff(cstr, 1, (char)ch);
         }
     }
+    endwin();
+    sprintf(cstr, "screen -S %s-%d -X quit", argv[1], uniq);
+    system(cstr);
+    sprintf(screenStuff, "screen -S %s -X quit", argv[1]);
+    system(screenStuff);
+    free(cstr);
     free(screenStuff);
+    close(btfd);
+    close(infobfd);
+    remove("./bt.tmp");
+    remove("./infob.tmp");
     return 0;
 }
--- main.sh
+++ main.sh
@@ -3,6 +3,8 @@
 # Requires Linux, Bash
 ###
 
+gcc -ggdb -lncurses -Wall -std=gnu11 gcon.c -o gcon.exe
+
 #get name of current xterm emulator
 XTERM=$(ps -p $(cat /proc/$PPID/stat | awk -F ' ' '{print $4}') | awk -F ' ' 'NR==2 {print $4}')
 #gnome-terminal shows up as 'gnome-terminal-' for whatever reason
@@ -28,16 +30,16 @@ fi
 
 if [ "$succeed" = "0" ]; then
     screen -S $winName -p 0 -X split &&\
-    screen -S $winName -p 0 -X resize 90% &&\
+    screen -S $winName -p 0 -X resize 95% &&\
     screen -S $winName -p 0 -X focus down &&\
     screen -S $winName -p 0 -X screen bash -i &&\
-    screen -S $winName -p 1 -X stuff "./gcon.exe ${winName}\n" &&\
+    screen -S $winName -p 1 -X stuff "./gcon.exe ${winName} ${tmp} \n" &&\
     screen -S $winName -p 0 -X focus right &&\
     screen -S $winName -p 0 -X split &&\
-    screen -S $winName -p 0 -X screen watch -n .2 -t -d cat bt.tmp &&\
+    screen -S $winName -p 0 -X screen watch -n .5 -t -d "cat bt.tmp | sed 's/\x00//g' | tee bt.tmp" &&\
     screen -S $winName -p 0 -X resize 70% &&\
     screen -S $winName -p 0 -X focus down &&\
-    screen -S $winName -p 0 -X screen watch -n .2 -t -d cat infob.tmp &&\
+    screen -S $winName -p 0 -X screen watch -n .5 -t -d "cat infob.tmp | sed 's/\x00//g' | tee infob.tmp" &&\
     screen -S $winName -p 0 -X focus left &&\
     screen -S $winName -p 0 -X focus down
 else
